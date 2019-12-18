7560d30c095e2f6b58838ea47868f26765a0775a
Cory Suida <itxdevcory@gmail.com>
2019-12-18 15:58:54 -0500

    commit test

---
+++ scripts.js
@@ -0,0 +1,7 @@
+﻿window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'UA-144274538-1');
+-webkit - text - stroke: 0.1px!important;
+text - stroke: 0.1px!important;
