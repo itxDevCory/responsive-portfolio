4484f6d1e42c202c5f5fd550702248d1c76882dd
Cory Suida <itxdevcory@gmail.com>
2020-01-22 09:09:33 -0500

    update bootstrap basic portfolio assignment

--- scripts.js
+++ scripts.js
@@ -1,7 +1,1 @@
-﻿window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-
-gtag('config', 'UA-144274538-1');
--webkit - text - stroke: 0.1px!important;
-text - stroke: 0.1px!important;
+﻿
